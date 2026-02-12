4282cd64e902278ed9cf9b3c74894c3a8f9011e7
SophiaBetzler <sophia.betzler@czii.org>
2026-02-12 12:47:59 -0800

    Add BasicFunctions.py with GUI initialization

---
+++ scripts/BasicFunctions.py
@@ -0,0 +1,9 @@
+
+from CoincidenceGUI import CoincidenceGUI
+from CoincidenceFunctions import CoincidenceFunctions
+
+
+
+from Basic_Functions import BasicFunctions, OverArch
+CoincidenceGUI(oa=OverArch, mode='manual')
+
